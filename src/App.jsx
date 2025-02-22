64b513d56f5ef1c8280fd6b488ff7c0cb02dfd8a
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-02-22 14:20:57 +0530

    feat: adds tailwind and shadcn for jsconfig this added the tailwind and shadcn configs compatable to JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import { useState } from 'react';
-import { invoke } from '@tauri-apps/api/core';
 import './App.css'
 
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-  async function handleClick() {
-    try {
-      // Invoke the Rust function
-      const response = await invoke('greet', { name: 'User' });
-      setGreeting(response);
-    } catch (error) {
-      console.error('Error calling Rust function:', error);
-    }
-  }
+
 
   return (
     
-    <div className="container">
+    <div className="container bg-black text-white">
     <h1>Tauri + React</h1>
-    <button onClick={handleClick}>
-      Call Rust Function
-    </button>
-    <div>
-    {greeting}
-    </div>
+    
+    
   </div>
 
   )
